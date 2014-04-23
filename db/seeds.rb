5292dc0606602e157e62ad4fa852878b38c1caec
Haris Mahmood <harismahmood89@gmail.com>
2014-04-23 18:30:45 -0400

    fixed seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,29 +6,30 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-category = Category.create([{name: 'Header'}])
+Category.create!(name: 'Header')
 
-bloc = Bloc.create([
-	{ name: 'header1'}, 
-	{ code: '<header></header>'},
-	{ category_id: 1},
-	{ img_url: 'http://placehold.it/800x150/e5e5e5'}
-])
+a = Bloc.create!(
+	name: 'header1', 
+	code: '<header></header>',
+	category: Category.find_by(name: "Header"),
+	img_url: 'http://placehold.it/800x150/e5e5e5'
+)
 
-bloc = Bloc.create([
-	{ name: 'header2'}, 
-	{ code: '<header></header>'},
-	{ category_id: 1},
-	{ img_url: 'http://placehold.it/800x150/333333'}
-])
-bloc = Bloc.create([
-	{ name: 'header3'}, 
-	{ code: '<header></header>'},
-	{ category_id: 1},
-	{ img_url: 'http://placehold.it/800x150/4d4d4d'}
-])
+b = Bloc.create!(
+	name: 'header2', 
+	code: '<header></header>',
+	category: Category.find_by(name: "Header"),
+	img_url: 'http://placehold.it/800x150/333333'
+)
 
-webpage = Webpage.create([
-	{ name: 'Website 01'},
-	{ bloc_id: [1,2,3]}
+c = Bloc.create!(
+	name: 'header3', 
+	code: '<header></header>',
+	category: Category.find_by(name: "Header"),
+	img_url: 'http://placehold.it/800x150/4d4d4d'
+)
+
+Webpage.create!([
+	name: 'Website 01',
+	blocs: [a, b, c]
 ])
