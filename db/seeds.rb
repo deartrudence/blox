dfe8396c4dab1626b66b1cd83c18c81d459db243
Haris Mahmood <harismahmood89@gmail.com>
2014-04-23 17:37:23 -0400

    Added seeds to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+category = Category.create([{name: 'Header'}])
+
+bloc = Bloc.create([
+	{ name: 'header1'}, 
+	{ code: '<header></header>'},
+	{ category_id: 1},
+	{ img_url: 'http://placehold.it/800x150/e5e5e5'}
+])
+
+bloc = Bloc.create([
+	{ name: 'header2'}, 
+	{ code: '<header></header>'},
+	{ category_id: 1},
+	{ img_url: 'http://placehold.it/800x150/333333'}
+])
+bloc = Bloc.create([
+	{ name: 'header3'}, 
+	{ code: '<header></header>'},
+	{ category_id: 1},
+	{ img_url: 'http://placehold.it/800x150/4d4d4d'}
+])
+
+webpage = Webpage.create([
+	{ name: 'Website 01'},
+	{ bloc_id: [1,2,3]}
+])
