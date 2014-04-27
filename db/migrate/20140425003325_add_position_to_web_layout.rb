1b81817c0ec0ac901ba9dfbfd71df21540138dd6
Haris Mahmood <harismahmood89@gmail.com>
2014-04-27 15:26:02 -0400

    hm: styles + js in place

--- db/migrate/20140425003325_add_position_to_web_layout.rb
+++
@@ -1,5 +0,0 @@
-class AddPositionToWebLayout < ActiveRecord::Migration
-  def change
-    add_column :web_layouts, :position, :integer
-  end
-end
--- db/migrate/20140426144657_drop_web_layout.rb
+++
@@ -1,5 +0,0 @@
-class DropWebLayout < ActiveRecord::Migration
-  def change
-  	drop_table :web_layouts
-  end
-end
