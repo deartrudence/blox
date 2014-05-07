63285b59bf76176b74f687cf657471e50fe335e0
deartrudence <trudy.macnabb@gmail.com>
2014-05-07 06:30:40 -0400

    tm - added sign in with Twitter/Omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,19 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable,
+         :omniauthable, omniauth_providers: [:twitter]
 
    has_many :webpages
+
+   def self.from_twitter_auth(auth)
+    #if a user exists in the system with auth.uid and auth.provider, return it.
+    #if a user doesn't exist in the system with auth.uid
+    #and auth.provider, create a new one
+    user = where(provider: auth.provider, uid: auth.uid).first_or_create(email: auth.info.name) 
+    user.update(token: auth.credentials.token, secret: auth.credentials.secret)
+    return user
+  end
+
+
 end
