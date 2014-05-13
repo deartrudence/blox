0162ac683b83c8178dd236fff68992473972dc05
deartrudence <trudy.macnabb@gmail.com>
2014-05-13 15:39:10 -0400

    tm - fixed search to work with 'whichstuff' param

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,18 +6,16 @@ class DashboardController < ApplicationController
   	@blocs = Bloc.all
   	@categories = Category.all
   	@webpages = Webpage.count
-  	@users = User.all
     @whichstuff = params[:whichstuff]
-  end
-
-  def show_user_list
-    @whichstuff = params[:whichstuff] 
     if params[:search]
       @users = User.search(params[:search])
     else
       @users = User.all
-    end
-    #redirect_to dashboard_show_path(:whichstuff => "userlist")
+    end 
+  end
+
+  def show_user_list 
+      @users = User.all
   end
 
 end
