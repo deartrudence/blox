2faa7e27346cb2d49eb83dcd133832da9f09c615
deartrudence <trudy.macnabb@gmail.com>
2014-06-02 22:29:05 -0400

    tm :zap: - added mandril config to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,17 @@ Blox::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.smtp_settings = {
+  :port =>           '587',
+  :address =>        'smtp.mandrillapp.com',
+  :user_name =>      ENV['MANDRILL_USERNAME'],
+  :password =>       ENV['MANDRILL_APIKEY'],
+  :domain =>         'http://www.buildweblox.com',
+  :authentication => :plain
+}
+
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.default_url_options = { host: "http://www.buildweblox.com" }
 end
