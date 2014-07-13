70e6caeaa88ae19f99d623716ea1cdd1972e7553
deartrudence <trudy.macnabb@gmail.com>
2014-07-13 18:03:27 -0400

    tm: added like button to bloc#show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,8 @@ module ApplicationHelper
     	"http://gravatar.com/avatar/#{gravatar_id}.png"
   	end
 
-  	def get_like_id(bloc,user)
-  		Like.where(:likeable_id => bloc, :user_id => user)
+  	def liked?(bloc,user)
+  		Like.where(:likeable_id => bloc, :user_id => user).any?
   	end
   
 end
