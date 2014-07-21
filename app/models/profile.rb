a5662b310a045840054495b3461a7eb3cb7f0734
deartrudence <trudy.macnabb@gmail.com>
2014-07-21 18:00:17 -0400

    tm: adding new styling the webpage views

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,7 +3,7 @@ class Profile < ActiveRecord::Base
 
   validates :user, presence: true
 
-  has_attached_file :avatar, :styles => { :thumb => "100x100>"}
+  has_attached_file :avatar, :styles => { :thumb => "100x100"}
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
